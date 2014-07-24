27d36c38208f7e734e71e398e9e2fd52e923dd68
Jordan Thayer <jthayer@jordans-mbp.wp.comcast.net>
2014-07-24 14:04:29 -0400

    Stubbing in the other readMdata files.  Improving the makefile a bit.

---
+++ transAss/readMData2.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author Seabass
+ * Purpose -- Read in a particular Mordor data file, MDATA11.MDR
+ */
+
+#include <iostream>
+#include <fstream>
+#include "mordorBinaryReader.hpp" // bring in the binary ready definitions
+
+static const string FNAME = "MDATA2.MDR";
+static const int SIZE = -1; // size of MDATA11.MDR in bytes
