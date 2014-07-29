4e85dfae7544e47da6460e37de4163b7eb8484f7
Seabass <jordan.thayer@gmail.com>
2014-07-29 19:46:44 -0400

    looks like we can run through mdata3

--- transAss/readMData3.cpp
+++ transAss/readMData3.cpp
@@ -94,6 +94,7 @@ struct record{
   int swings;             //0x12
   specialItemMask sim;    //starting at 0x14
   int spellID;            //0x18 -- cross referenced with MDATA2.MDR
+  int spellID2;            //0x18 -- cross referenced with MDATA2.MDR
   int chargePerCast;      //0x1A
   //unused at 0x1C
   guildMask gm;           //startng at 0x1E
@@ -152,40 +153,165 @@ abilities readAbilityMask(ifstream *mdata){
   return ret;
 }
 
+void printGuildMask(const guildMask *ret){
+  cout << "Useable by: ";
+
+  if(ret->nomad){
+    cout << "Nomad ";
+  }
+  if(ret->warrior){
+    cout << "Warrior ";
+  }
+  if(ret->paladin){
+    cout << "Paladin ";
+  }
+  if(ret->ninja){
+    cout << "Ninja ";
+  }
+  if(ret->villain){
+    cout << "Villain ";
+  }
+  if(ret->seeker){
+    cout << "Seeker ";
+  }
+  if(ret->thief){
+    cout << "Thief ";
+  }
+  if(ret->scavenger){
+    cout << "Scavenger ";
+  }
+  if(ret->mage){
+    cout << "Mage ";
+  }
+  if(ret->sorceror){
+    cout << "Sorceror ";
+  }
+  if(ret->wizard){
+    cout << "Wizard ";
+  }
+  if(ret->healer){
+    cout << "Healer ";
+  }
+  cout << endl;
+}
+
 guildMask readGuildMask(ifstream *mdata){
-  BYTE b1 = readByte(mdata);
-  BYTE b2 = readByte(mdata);
-  BYTE b3 = readByte(mdata);
+  WORD data = readWord(mdata);
   guildMask ret;
 
   // I don't know if any of these are simultaneously set, so fill out the mask
-  ret.nomad = b1 ^ 0x01;
-  ret.warrior = b1 ^ 0x02;
-  ret.paladin = b1 ^ 0x04;
-  ret.ninja = b1 ^ 0x08;
+  ret.nomad = data & 0x001;
+  ret.warrior = data & 0x002;
+  ret.paladin = data & 0x004;
+  ret.ninja = data & 0x008;
   
-  ret.villain = b2 ^ 0x01;
-  ret.seeker = b2 ^ 0x02;
-  ret.thief = b2 ^ 0x04;
-  ret.scavenger = b2 ^ 0x08;
+  ret.villain = data & 0x010;
+  ret.seeker = data & 0x020;
+  ret.thief = data & 0x040;
+  ret.scavenger = data & 0x080;
 
-  ret.mage = b3 ^ 0x01;
-  ret.sorceror = b3 ^ 0x02;
-  ret.wizard = b3 ^ 0x04;
-  ret.healer = b3 ^ 0x08;
+  ret.mage = data & 0x100;
+  ret.sorceror = data & 0x200;
+  ret.wizard = data & 0x400;
+  ret.healer = data & 0x800;
 
+  printGuildMask(&ret);
   return ret;
+}
+
+ guildMask readGuildMaskOld(ifstream *mdata){
+  BYTE b1 = readByte(mdata);
+  BYTE b2 = readByte(mdata);
+  BYTE b3 = readByte(mdata);
+
+  guildMask ret;
+
+  ret.nomad = b1 & 0x1;
+  ret.warrior = b1 & 0x2;
+  ret.paladin = b1 & 0x4;
+  ret.ninja = b1 & 0x8;
+
+  ret.villain = b2 & 0x1;
+  ret.seeker = b2 & 0x2;
+  ret.thief = b2 & 0x4;
+  ret.scavenger = b2 & 0x8;
+
+  ret.mage = b3 & 0x1;
+  ret.sorceror = b3 & 0x2;
+  ret.wizard = b3 & 0x4;
+  ret.healer = b3 & 0x8;
+ 
+  printGuildMask(&ret);
+   return ret;
+ }
+
+void printAlignmentMask(const alignmentMask *ret){
+  cout << "Useable by: ";
+  if(ret->unaligned){
+    cout << "Unaligned ";
+  }
+  if(ret->good){
+    cout << "Good ";
+  }
+  if(ret->neutral){
+    cout << "Neutral ";
+  }
+  if(ret->evil){
+    cout << "Evil ";
+  }
+  cout << endl;
 }
 
 alignmentMask readAlignmentMask(ifstream *mdata){
   BYTE b1 = readByte(mdata);
   alignmentMask ret;
   
-  ret.unaligned = b1 ^ 0x01;
-  ret.good = b1 ^ 0x02;
-  ret.neutral = b1 ^ 0x04;
-  ret.evil = b1 ^ 0x08;
+  ret.unaligned = b1 & 0x01;
+  ret.good = b1 & 0x02;
+  ret.neutral = b1 & 0x04;
+  ret.evil = b1 & 0x08;
+  
+  printAlignmentMask(&ret);
   return ret;
+}
+
+void printResistanceMask(const resistanceMask *ret){
+  cout << "Resists: ";
+
+  if(ret->fire){
+    cout << "Fire ";
+  }
+  if(ret->cold){
+    cout << "Cold ";
+  }
+  if(ret->electrical){
+    cout << "Electrical ";
+  }
+  if(ret->mind){
+    cout << "Mind ";
+  }
+  if(ret->disease){
+    cout << "Disease ";
+  }
+  if(ret->poison){
+    cout << "Poison ";
+  }
+  if(ret->magic){
+    cout << "Magic ";
+  }
+  if(ret->stone){
+    cout << "Stone ";
+  }
+  if(ret->paralysis){
+    cout << "Paralysis ";
+  }
+  if(ret->drain){
+    cout << "Drain ";
+  }
+  if(ret->acid){
+    cout << "Acid ";
+  }
+ cout << endl;
 }
 
 resistanceMask readResistances(ifstream *mdata){
@@ -194,23 +320,32 @@ resistanceMask readResistances(ifstream *mdata){
   BYTE b3 = readWord(mdata);
   resistanceMask ret;
 
-  ret.fire = b1 ^ 0x01;
-  ret.cold = b1 ^ 0x02;
-  ret.electrical = b1 ^ 0x04;
-  ret.mind = b1 ^ 0x08;
+  ret.fire = b1 & 0x01;
+  ret.cold = b1 & 0x02;
+  ret.electrical = b1 & 0x04;
+  ret.mind = b1 & 0x08;
   
-  ret.disease = b2 ^ 0x01;
-  ret.poison = b2 ^ 0x02;
-  ret.magic = b2 ^ 0x04;
-  ret.stone = b2 ^ 0x08;
+  ret.disease = b2 & 0x01;
+  ret.poison = b2 & 0x02;
+  ret.magic = b2 & 0x04;
+  ret.stone = b2 & 0x08;
 
-  ret.paralysis = b3 ^ 0x01;
-  ret.drain = b3 ^ 0x02;
-  ret.acid = b3 ^ 0x04;
+  ret.paralysis = b3 & 0x01;
+  ret.drain = b3 & 0x02;
+  ret.acid = b3 & 0x04;
 
+  printResistanceMask(&ret);
   return ret;
 }
 
+void printStatSet(const statSet *ret){
+  cout << "STR: " << ret->strength << endl;
+  cout << "INT: " << ret->intelligence << endl;
+  cout << "WIS: " << ret->wisdom << endl;
+  cout << "CON: " << ret->constitution << endl;
+  cout << "CHA: " << ret->charisma << endl;
+  cout << "DEX: " << ret->dexterity << endl;
+}
 statSet readStatSet(ifstream *mdata){
   statSet ret;
   // these may need to be signed words instead of unsigned words
@@ -220,7 +355,21 @@ statSet readStatSet(ifstream *mdata){
   ret.constitution = readWord(mdata);
   ret.charisma = readWord(mdata);
   ret.dexterity = readWord(mdata);
+
+  printStatSet(&ret);
   return ret;
+}
+
+void printCurseMask(const curseMask *ret){
+  if(ret->notCursed){
+    cout << "Not Cursed" << endl;
+  }
+  if(ret->cursedNoEquip){
+    cout << "Cursed" << endl;
+  }
+  if(ret->cursedAutoEquip){
+    cout << "Cursed, Automatically Equips" << endl;
+  }
 }
 
 curseMask readCurseMask(ifstream *mdata){
@@ -232,7 +381,11 @@ curseMask readCurseMask(ifstream *mdata){
   ret.cursedNoEquip = b1 == 1;
   ret.cursedAutoEquip = b1 == 2;
 
+  printCurseMask(&ret);
   return ret;
+}
+
+void printSpecialItemMask(const specialItemMask *ret){
 }
 
 specialItemMask readSpecialItemMask(ifstream *mdata){
@@ -241,7 +394,7 @@ specialItemMask readSpecialItemMask(ifstream *mdata){
   //ageReducingItem 0x0D to 0x0E        -- just a word
   //guild crest 0xC0 to 0xC9            -- 10 values -- guild mask?
   specialItemMask ret; 
-
+  WORD data = readWord(mdata);
   
   //fixme stub
   return ret;
@@ -298,8 +451,14 @@ int readItemClass(ifstream *mdata){
 0x400 = Acid res
 */
 
-  //fixme stub
-  return -1;
+    WORD classID = readWord(mdata);
+    switch(classID){
+    case 0x00:
+      cout << "Found hands" << endl;
+      return 0x00;
+      break;
+    default: return -1;
+    }
 }
 
 void printItem(ifstream *mdata){
@@ -311,28 +470,54 @@ record readItem(ifstream *mdata){
   
   ret.name = readVBString(mdata);
   cout << "Reading: " << ret.name << endl;
+  int offset = 7; 
+  printLoc(mdata, offset, "ID");
   ret.id = readWord(mdata);                   //0x00
+  printLoc(mdata, offset, "Attack");  
   ret.attackMod = readWord(mdata);
+  printLoc(mdata, offset, "Defence");
   ret.defenceMod = readWord(mdata);
+  printLoc(mdata, offset, "Value");
   ret.itemValue = readInt(mdata);
+  
 
   cout << "Cost: " << ret.itemValue << endl;
-
+  printLoc(mdata, offset, "Earliest");
   ret.earliestFindLevel = readWord(mdata);
+  printLoc(mdata, offset, "Find%");
   ret.chanceofFinding = readWord(mdata);
+  printLoc(mdata, offset, "Ability Mask");
   ret.abilityMask = readAbilityMask(mdata);
+  
+  assert(checkAlignment(mdata));
+  printLoc(mdata, offset, "Swings");  
   ret.swings = readWord(mdata);
+  printLoc(mdata, offset, "Special Item Mask");    
   ret.sim = readSpecialItemMask(mdata);
+  printLoc(mdata, offset, "Spell ID");      
   ret.spellID = readWord(mdata);
+  printLoc(mdata, offset, "ID Spell");      
+  ret.spellID2 = readWord(mdata);
+  printLoc(mdata, offset, "Charges");      
   ret.chargePerCast= readWord(mdata);
+  printLoc(mdata, offset, "Guild Mask");      
   assert(checkAlignment(mdata));              //0x1C
+  
   ret.gm = readGuildMask(mdata);
+  
   assert(checkAlignment(mdata));              //0x20
+  printLoc(mdata, offset, "Level Scale");      
   ret.levelScaleFactor = readWord(mdata);
-  ret.damageModifier = readWord(mdata);
+  printLoc(mdata, offset, "Damage Mod");      
+  ret.damageModifier = readInt(mdata);
+  printLoc(mdata, offset, "Alignment Mask");      
   ret.am = readAlignmentMask(mdata);
   assert(checkAlignment(mdata));              //0x2A
+
   ret.numHands = readWord(mdata);
+  
+  cout << "Number of hands: " << ret.numHands << endl;
+
   ret.itemClass = readItemClass(mdata);
   assert(checkAlignment(mdata));              //0x32
   ret.statReqs = readStatSet(mdata);
