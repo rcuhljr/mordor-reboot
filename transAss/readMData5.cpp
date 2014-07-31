f2b0ceec84abaeb8815e4678b8d398a52a170a32
Seabass <jordan.thayer@gmail.com>
2014-07-31 18:29:00 -0400

    Sketched out monster structs

--- transAss/readMData5.cpp
+++ transAss/readMData5.cpp
@@ -2,10 +2,51 @@
 #include <iostream>
 #include <fstream>
 #include "mdataTools.hpp"
+#include "mordorBinaryReader.hpp"
 #include "readMData5.hpp"
+#include "assert.h"
 
 using namespace std;
 
+struct header{
+  char* version;
+  WORD numMonsters;
+};
+
+struct monster{
+  char* name;
+  WORD attackRating;
+  WORD defenceRating;
+  WORD id;
+  WORD hitPoints;
+  WORD groupSize;
+  WORD portaitID;
+  WORD u8;
+  WORD appearOnLevel;
+  WORD fireResistance;
+  WORD coldResistance;
+  WORD electricalResistance;
+  WORD mindResistance;
+  WORD diseaseResistance;
+  WORD poisonResistance;
+  WORD magicResistance;
+  WORD stoneResistance;
+  WORD paralysisResistance;
+  WORD drainResistance;
+  WORD acidResistance;
+  WORD u21, u22, u23, u24, u25, u26, u27;
+  WORD encounterChance;
+  WORD nothingChance;
+  WORD boxChance;
+  WORD chestChance;
+  WORD u32, u33, u34, u35, u36, u37, u38;
+  WORD strength;
+  WORD intelligence;
+  WORD wisdom;
+  WORD constitution;
+  WORD dexterity;
+  WORD u45, u46, u47, u48, u49, u50, u51, u52;
+};
 
 int main(int argc, char** argv){
   char* datAbsolutePath;
