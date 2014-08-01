6a47d834a660c1f5797e93f2239a90573f7d3089
Seabass <jordan.thayer@gmail.com>
2014-08-01 09:37:02 -0400

    stubbed out other structs needed

--- transAss/readMData11.hpp
+++ transAss/readMData11.hpp
@@ -1,13 +1,42 @@
 #ifndef READ_MDATA_11
 #define READ_MDATA_11
+#include "mordorBinaryReader.hpp"
 
 struct levelHeader {
-  unsigned short width;
-  unsigned short height;
-  unsigned short levelNumber;
-  unsigned short numAreas;
-  unsigned short numChutes;
-  unsigned short numTeleports;
+  WORD width;
+  WORD height;
+  WORD levelNumber;
+  WORD numAreas;
+  WORD numChutes;
+  WORD numTeleports;
+};
+
+struct fieldRecord {
+  WORD spawnAreaID;
+  BYTE fieldMask[];    // 8 bytes
+};
+
+struct countHeader { //area, teleport, chute header, level offset
+  WORD count;
+};
+
+struct areaRecord {
+  DWORD spawnTypeMask; // 4 bytes
+  WORD lairID;
+};
+
+struct teleporterRecord{
+  WORD srcX;
+  WORD srcY;
+  WORD destX;
+  WORD destY;
+  WORD destZ;
+};
+
+struct chuteRecord {
+  WORD x;
+  WORD y;
+  WORD dropDepth;
 };
 
 #endif
