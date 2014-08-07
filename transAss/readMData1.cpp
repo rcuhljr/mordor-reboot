6ef2fd83410fd80f43f41f96f85170ff6138a489
Seabass <jordan.thayer@gmail.com>
2014-08-07 19:20:06 -0400

    moved struct definitions into header files

--- transAss/readMData1.cpp
+++ transAss/readMData1.cpp
@@ -20,44 +20,7 @@ struct header{
   WORD numMonsterTypes;
 };
 
-struct race{
-  char* name;
-  WORD minStats;
-  WORD maxStats;
-  WORD resistances;
-  DWORD alignment;
-  WORD size;
-  WORD bonusPoints;
-  WORD maxAge;
-  DWORD expFactor;
-};
 
-struct guild{
-  char* name;
-  WORD averageHits;
-  WORD maxLevel;
-  WORD MH;               // ??
-  DWORD expFactor;
-  WORD u3;               // ??
-  WORD reqStats;
-  DWORD alignment;
-  DWORD abilityRates;
-  WORD u7;               // ??
-  DWORD u8;              // ??
-  WORD questPercentage;
-  DWORD spellStuff;
-  DWORD raceMask;
-  WORD u12;               //??
-  DWORD levelMod;         //??
-  DWORD u13, u14, u15;   // ??
-  WORD u16, u17;         // ??
-};
-
-// works for sub item, item, sub monster, monster
-struct namedTypeID{
-  char* name;
-  WORD type;
-};
 
 void printFileHeader(header *ret){
   cout << "Version:              " << ret->version << endl;
--- transAss/readMData1.hpp
+++ transAss/readMData1.hpp
@@ -1,4 +1,45 @@
 #ifndef READ_MDATA_1
 #define READ_MDATA_1
 
+#include "mordorBinaryReader.hpp"
+
+struct race{
+  char* name;
+  WORD minStats;
+  WORD maxStats;
+  WORD resistances;
+  DWORD alignment;
+  WORD size;
+  WORD bonusPoints;
+  WORD maxAge;
+  DWORD expFactor;
+};
+
+struct guild{
+  char* name;
+  WORD averageHits;
+  WORD maxLevel;
+  WORD MH;               // ??
+  DWORD expFactor;
+  WORD u3;               // ??
+  WORD reqStats;
+  DWORD alignment;
+  DWORD abilityRates;
+  WORD u7;               // ??
+  DWORD u8;              // ??
+  WORD questPercentage;
+  DWORD spellStuff;
+  DWORD raceMask;
+  WORD u12;               //??
+  DWORD levelMod;         //??
+  DWORD u13, u14, u15;   // ??
+  WORD u16, u17;         // ??
+};
+
+// works for sub item, item, sub monster, monster
+struct namedTypeID{
+  char* name;
+  WORD type;
+};
+
 #endif
