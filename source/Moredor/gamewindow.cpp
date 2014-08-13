c8a3ebdd0ce345486f17ec9cb15f7e1eccaa8715
Robert C. Uhl <rcuhl@sep.com>
2014-08-13 12:29:55 -0400

    Remove unused resources and headers.

--- source/Moredor/gamewindow.cpp
+++ source/Moredor/gamewindow.cpp
@@ -1,8 +1,6 @@
 #include "gamewindow.h"
 #include "ui_gamewindow.h"
 #include "createcharacterwidget.h"
-#include <QUiLoader>
-#include <QFile>
 #include <QDebug>
 
 GameWindow::GameWindow(QWidget *parent) :
@@ -19,19 +17,7 @@ GameWindow::~GameWindow()
 
 void GameWindow::on_createCharacterButton_clicked()
 {
-//    QUiLoader loader;
-//    QFile file(":/forms/createcharacterwidget.ui");
-//    file.open(QFile::ReadOnly);
-//    QWidget *createCharacterWidget = loader.load(&file, this);
-//    file.close();
-
-//    //setCentralWidget(createCharacterWidget);
-//    createCharacterWidget->
-//    createCharacterWidget->show();
-
     QDockWidget *dock = new CreateCharacterWidget(this);
 
     dock->show();
-
-
 }
