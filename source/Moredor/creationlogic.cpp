c6e8da9a380295fd0d0de0bb68965d68d1973e25
Robert C. Uhl <rcuhl@sep.com>
2014-08-27 15:43:30 -0400

    ugh.

--- source/Moredor/creationlogic.cpp
+++ source/Moredor/creationlogic.cpp
@@ -1,15 +1,66 @@
 #include "creationlogic.h"
-#include <string>
 #include <QStringList>
+#include <Qmap>
+#include <QList>
 
 CreationLogic::CreationLogic()
 {
+    //#TODO Read data from Json
+    int nomadStats[6] = {1,1,1,1,1,1};
+    bool nomadAlign[3] = {true,true,true};
+
+    int warriorStats[6] = {14,7,5,10,3,8};
+    bool warriorAlign[3] = {true,true,true};
+
+    int thiefStats[6] = {8,12,8,6,5,17};
+    bool thiefAlign[3] = {false,true,false};
+
+    GUILD* nomad = new GUILD(nomadStats, nomadAlign);
+    GUILD* warrior = new GUILD(warriorStats, warriorAlign);
+    GUILD* thief = new GUILD(thiefStats, thiefAlign);
+
+    nomad->name = QString("Nomad");
+    warrior->name = QString("Warrior");
+    thief->name = QString("Thief");
+
+
+    int humanStart[6] = {4,4,4,6,5,6};
+    int humanMax[6] = {17,18,18,17,18,18};
+    bool humanAlign[3] = {true,true,true};
+
+
+    RACE* human = new RACE(humanAlign, humanStart, humanMax);
+    human->name = QString("Human");
+    QList<GUILD> humanGuilds;
+    humanGuilds.push_back(*nomad);
+    humanGuilds.push_back(*warrior);
+    humanGuilds.push_back(*thief);
+    human->guilds = humanGuilds;
+
+    races[human->name] = *human;
+
+    int giantStart[6] = {12,4,3,9,2,3};
+    int giantMax[6] =  {25,17,17,19,16,18};
+    bool giantAlign[3] = {true,true,true};
+
+
+    RACE* giant = new RACE(giantAlign, giantStart, giantMax);
+    giant->name = QString("Giant");
+    QList<GUILD> giantGuilds;
+    giantGuilds.push_back(*nomad);
+    giantGuilds.push_back(*warrior);
+    giant->guilds = giantGuilds;
+
+    races[giant->name] = *giant;
 }
 
 
 QStringList CreationLogic::availableRaces(){
-    QStringList races;
-    races.push_back(QString("human"));
-    races.push_back(QString("elf"));
-    return races;
+    QStringList raceStrings;
+    QMap<QString,RACE>::const_iterator i = races.constBegin();
+    while(i != races.constEnd()){
+        raceStrings.push_back(i.key());
+        i++;
+    }
+    return raceStrings;
 }
--- source/Moredor/creationlogic.h
+++ source/Moredor/creationlogic.h
@@ -1,12 +1,47 @@
 #ifndef CREATIONLOGIC_H
 #define CREATIONLOGIC_H
 #include <QStringList>
+#include <QMap>
+#include <QString>
 
 class CreationLogic
 {
 public:
+    enum Alignments {Good, Neutral, Evil};
+    enum Stats {STR,INT,WIS,CON,CHA,DEX};
+
+    struct GUILD {
+        QString name;
+        std::vector<int> requirements;
+        std::vector<bool> alignments;
+
+        GUILD(int *reqs, bool *align){
+            requirements = std::vector<int>(reqs, reqs + sizeof(reqs)/sizeof(int) );
+            alignments = std::vector<bool>(align, align + sizeof(align)/sizeof(bool) );
+        }
+    };
+
+    struct RACE {
+        QString name;
+        std::vector<bool> alignments;
+        std::vector<int> startingStats;
+        std::vector<int> maxStats;
+        QList<GUILD> guilds;
+
+        RACE(bool *align, int *start, int *max){
+            alignments = std::vector<bool>(align, align + sizeof(align)/sizeof(bool) );
+            startingStats = std::vector<int>(start, start + sizeof(start)/sizeof(int) );
+            maxStats = std::vector<int>(max, max + sizeof(max)/sizeof(int) );
+
+        }
+
+        RACE(){}
+    };
     CreationLogic();
     QStringList availableRaces();
+
+private:
+    QMap<QString,RACE> races;
 };
 
 #endif // CREATIONLOGIC_H
